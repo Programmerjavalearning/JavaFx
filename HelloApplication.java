362b447a05dfa314ccab1b3cf46264472a8e5f7d
Nicolas <112377899+Programmerjavalearning@users.noreply.github.com>
2022-09-05 15:19:21 +0200

    Add a label

--- HelloApplication.java
+++ HelloApplication.java
@@ -3,18 +3,26 @@ package com.example.hellojavafx;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Label myLabel;
+    private Label myLabel2;
     @Override
     public void start(Stage primaryStage) throws IOException {
 
         primaryStage.setTitle("Hello JavaFx");
+        myLabel = new Label();
+        myLabel2 = new Label();
+        myLabel.setText("Hello i'm a label");
+        myLabel2.setText("Hello i'm a label 2");
         Button button = new Button();
         button.setText("Click Me!");
         button.setOnAction(new EventHandler<ActionEvent>() {
@@ -25,9 +33,15 @@ public class HelloApplication extends Application {
 
             }
         });
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-        Scene scene = new Scene(root, 250, 200);
+
+        FlowPane flowPaneRoot = new FlowPane(10,10);
+        flowPaneRoot.setAlignment(Pos.CENTER);
+
+       // StackPane root = new StackPane();
+        flowPaneRoot.getChildren().add(button);
+        flowPaneRoot.getChildren().add(myLabel);
+        flowPaneRoot.getChildren().add(myLabel2);
+        Scene scene = new Scene(flowPaneRoot, 250, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
 
